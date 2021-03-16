d0e7f1151e0ee8d340d50a847599c06d18cf0997
Emily Poligerinos <79431404+poli0048-e@users.noreply.github.com>
2021-03-16 19:54:32 +1030

    second commit for C2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.poli0048',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.poli0048\n\nA docassemble extension.\n\n## Author\n\nEmily Poligerinos, poli0048@flinders.edu.au\n\n',
+      long_description='# docassemble.poli0048\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nEmily Poligerinos, poli0048@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Emily Poligerinos',
       author_email='poli0048@flinders.edu.au',
